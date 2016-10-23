49b576298409df02669733ee8c6e64a335ec27b8
Pavel Korshunov <pavel.korshunov@idiap.ch>
2016-10-23 15:47:53 +0200

    Better fix of sphinx issue

--- bob/bio/base/algorithm/__init__.py
+++ bob/bio/base/algorithm/__init__.py
@@ -5,13 +5,35 @@ from .LDA import LDA
 from .PLDA import PLDA
 from .BIC import BIC
 
-# to fix sphinx warnings of not being able to find classes, when path is shortened
-Algorithm.__module__ = "bob.bio.base.algorithm"
-Distance.__module__ = "bob.bio.base.algorithm"
-PCA.__module__ = "bob.bio.base.algorithm"
-LDA.__module__ = "bob.bio.base.algorithm"
-PLDA.__module__ = "bob.bio.base.algorithm"
-BIC.__module__ = "bob.bio.base.algorithm"
+# # to fix sphinx warnings of not being able to find classes, when path is shortened
+# Algorithm.__module__ = "bob.bio.base.algorithm"
+# Distance.__module__ = "bob.bio.base.algorithm"
+# PCA.__module__ = "bob.bio.base.algorithm"
+# LDA.__module__ = "bob.bio.base.algorithm"
+# PLDA.__module__ = "bob.bio.base.algorithm"
+# BIC.__module__ = "bob.bio.base.algorithm"
 
 # gets sphinx autodoc done right - don't remove it
+def __appropriate__(*args):
+  """Says object was actually declared here, and not in the import module.
+  Fixing sphinx warnings of not being able to find classes, when path is shortened.
+  Parameters:
+
+    *args: An iterable of objects to modify
+
+  Resolves `Sphinx referencing issues
+  <https://github.com/sphinx-doc/sphinx/issues/3048>`
+  """
+
+  for obj in args: obj.__module__ = __name__
+
+__appropriate__(
+    Algorithm,
+    Distance,
+    PCA,
+    LDA,
+    PLDA,
+    BIC,
+    )
+
 __all__ = [_ for _ in dir() if not _.startswith('_')]
--- bob/bio/base/preprocessor/__init__.py
+++ bob/bio/base/preprocessor/__init__.py
@@ -6,4 +6,21 @@ Preprocessor.__module__ = "bob.bio.base.preprocessor"
 Filename.__module__ = "bob.bio.base.preprocessor"
 
 # gets sphinx autodoc done right - don't remove it
+def __appropriate__(*args):
+  """Says object was actually declared here, and not in the import module.
+  Fixing sphinx warnings of not being able to find classes, when path is shortened.
+  Parameters:
+
+    *args: An iterable of objects to modify
+
+  Resolves `Sphinx referencing issues
+  <https://github.com/sphinx-doc/sphinx/issues/3048>`
+  """
+
+  for obj in args: obj.__module__ = __name__
+
+__appropriate__(
+    Preprocessor,
+    Filename,
+    )
 __all__ = [_ for _ in dir() if not _.startswith('_')]
