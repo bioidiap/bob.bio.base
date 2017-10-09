c4c770d0a130e9eb214c6e806d1a228b651b7124
Manuel Gunther <siebenkopf@googlemail.com>
2017-10-09 11:36:07 -0600

    Testing single config file

--- bob/bio/base/test/dummy/config.py
+++ bob/bio/base/test/dummy/config.py
@@ -1,7 +1,7 @@
-from .database import database
-from .preprocessor import preprocessor
-from .extractor import extractor
-from .algorithm import algorithm
+from bob.bio.base.test.dummy.database import database
+from bob.bio.base.test.dummy.preprocessor import preprocessor
+from bob.bio.base.test.dummy.extractor import extractor
+from bob.bio.base.test.dummy.algorithm import algorithm
 zt_norm = True
 verbose = 1
 sub_directory = "test_dummy"
