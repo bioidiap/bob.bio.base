a4b2e31df1da7a2206f591122e7e278db448cf6e
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2018-01-25 12:13:15 +0100

    use a warning instead of an error

--- bob/bio/base/utils/resources.py
+++ bob/bio/base/utils/resources.py
@@ -131,6 +131,9 @@ def load_resource(resource, keyword, imports = ['bob.bio.base'], package_prefix=
   if os.path.isfile(resource):
     return read_config_file([resource], keyword)
 
+  if keyword not in valid_keywords:
+    logger.warning("The given keyword '%s' is not valid. Please use one of %s!", keyword, valid_keywords)
+
   # now, we check if the resource is registered as an entry point in the resource files
   entry_points = [entry_point for entry_point in _get_entry_points(keyword, package_prefix=package_prefix) if entry_point.name == resource]
 
