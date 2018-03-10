97e70380ba2702480377148b66b4bc30bf3bb409
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2018-03-10 09:51:38 +0100

    put the simple annotator back

--- bob/bio/base/test/dummy/annotator.py
+++ bob/bio/base/test/dummy/annotator.py
@@ -2,16 +2,18 @@ from random import random
 from bob.bio.base.annotator import FailSafe, Callable
 
 
+def simple_annotator(image, **kwargs):
+    return {
+        'topleft': (0, 0),
+        'bottomright': image.shape,
+    }
+
+
 def moody_annotator(image, **kwargs):
+    annot = simple_annotator(image, **kwargs)
     if random() < 0.5:
-        return {
-            'topleft': (0, 0),
-        }
-    else:
-        return {
-            'topleft': (0, 0),
-            'bottomright': image.shape,
-        }
+        del annot['bottomright']
+    return annot
 
 
 def fail_annotator(image, **kwargs):
@@ -20,6 +22,6 @@ def fail_annotator(image, **kwargs):
 
 annotator = FailSafe(
     [Callable(fail_annotator),
-     Callable(moody_annotator)],
+     Callable(simple_annotator)],
     required_keys=['topleft', 'bottomright'],
 )
