8a99bcb2a70c6525d0d0c23224f78e38a49f6abf
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2018-05-07 10:10:22 +0200

    Set io-big flag for the demanding grid config

--- bob/bio/base/config/grid/demanding.py
+++ bob/bio/base/config/grid/demanding.py
@@ -4,13 +4,13 @@ import bob.bio.base
 grid = bob.bio.base.grid.Grid(
   training_queue = '32G',
   # preprocessing
-  preprocessing_queue = '4G',
+  preprocessing_queue = '4G-io-big',
   # feature extraction
-  extraction_queue = '8G',
+  extraction_queue = '8G-io-big',
   # feature projection
-  projection_queue = '8G',
+  projection_queue = '8G-io-big',
   # model enrollment
-  enrollment_queue = '8G',
+  enrollment_queue = '8G-io-big',
   # scoring
-  scoring_queue = '8G'
+  scoring_queue = '8G-io-big'
 )
