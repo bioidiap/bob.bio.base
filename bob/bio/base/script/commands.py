a0bb138d1f5019408dbdc1578b76f5705f076385
Theophile GENTILHOMME <tgentilhomme@jurasix08.idiap.ch>
2018-07-02 16:45:22 +0200

    Add definitions

--- bob/bio/base/script/commands.py
+++ bob/bio/base/script/commands.py
@@ -32,7 +32,8 @@ def rank_option(**kwargs):
 @common_options.metrics_command(common_options.METRICS_HELP.format(
     names='FtA, FAR, FRR, FMR, FMNR, HTER',
     criteria=CRITERIA, score_format=SCORE_FORMAT,
-    hter_note='Note that HTER = (FMR + FMNR) / 2',
+    hter_note='Note that FAR = FMR * (1 - FtA), FRR = FtA + FMNR * (1 - FtA) '
+    'and HTER = (FMR + FMNR) / 2',
     command='bob bio metrics'), criteria=CRITERIA)
 @common_options.cost_option()
 def metrics(ctx, scores, evaluation, **kwargs):
