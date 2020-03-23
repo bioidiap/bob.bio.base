00382740a72368ddb5231df10e53f4f3e5858fbe
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2020-03-23 11:05:53 +0100

    Removed openBR scoring
    Removed openBR scoring

--- bob/bio/base/score/__init__.py
+++ bob/bio/base/score/__init__.py
@@ -2,5 +2,3 @@ from .load import (open_file, split, cmc, split_four_column, four_column,
                    split_five_column, five_column, scores, load_score,
                    dump_score, cmc_four_column, cmc_five_column
                   )
-from .openbr import (write_matrix, write_score_file)
-
