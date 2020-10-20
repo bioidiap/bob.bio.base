ee8a62a779c1495b865413c080ec7f1b8211d788
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2020-10-20 19:34:15 +0200

    Make BioFile's self load data and annotations

--- bob/bio/base/database/file.py
+++ bob/bio/base/database/file.py
@@ -1,33 +1,80 @@
-#!/usr/bin/env python
-# vim: set fileencoding=utf-8 :
-
 import bob.db.base
+from bob.db.base.annotations import read_annotation_file
+from bob.pipelines.sample import _ReprMixin
 
+class BioFile(bob.db.base.File, _ReprMixin):
+    """
+    A simple base class that defines basic properties of File object for the use
+    in verification experiments
 
-class BioFile(bob.db.base.File):
-    """A simple base class that defines basic properties of File object for the use in verification experiments
-
-    Parameters
+    Attributes
     ----------
-
-    client_id : object
-      The id of the client this file belongs to.
-      Its type depends on your implementation.
-      If you use an SQL database, this should be an SQL type like Integer or String.
-
-    file_id : object
-      see :py:class:`bob.db.base.File` constructor
-
+    client_id : str or int
+        The id of the client this file belongs to.
+        Its type depends on your implementation.
+        If you use an SQL database, this should be an SQL type like Integer or
+        String.
     path : object
-      see :py:class:`bob.db.base.File` constructor
+        see :py:class:`bob.db.base.File` constructor
+    file_id : object
+        see :py:class:`bob.db.base.File` constructor
+    original_directory : str or None
+        The path to the original directory of the file
+    original_extension : str or None
+        The extension of the original files. This attribute is deprecated.
+        Please try to include the extension in the ``path`` attribute
+    annotation_directory : str or None
+        The path to the directory of the annotations
+    annotation_extension : str or None
+        The extension of annotation files. Default is ``.json``
+    annotation_type : str or None
+        The type of the annotation file, see
+        :any:`bob.db.base.annotations.read_annotation_file`. Default is
+        ``json``.
     """
 
-    def __init__(self, client_id, path, file_id=None, **kwargs):
+    def __init__(
+        self,
+        client_id,
+        path,
+        file_id=None,
+        original_directory=None,
+        original_extension=None,
+        annotation_directory=None,
+        annotation_extension=None,
+        annotation_type=None,
+        **kwargs,
+    ):
         super(BioFile, self).__init__(path, file_id, **kwargs)
 
         # just copy the information
         self.client_id = client_id
         """The id of the client, to which this file belongs to."""
+        self.original_directory = original_directory
+        self.original_extension = original_extension
+        self.annotation_directory = annotation_directory
+        self.annotation_extension = annotation_extension or ".json"
+        self.annotation_type = annotation_type or "json"
+
+    def load(self):
+        """Loads the data at the specified location and using the given extension.
+        Override it if you need to load differently.
+
+        Returns
+        -------
+        object
+            The loaded data (normally :py:class:`numpy.ndarray`).
+        """
+        # get the path
+        path = self.make_path(
+            self.original_directory or "", self.original_extension or ""
+        )
+        return bob.io.base.load(path)
+
+    @property
+    def annotations(self):
+        path = self.make_path(self.annotation_directory or "", self.annotation_extension or "")
+        return read_annotation_file(path, annotation_type=self.annotation_type)
 
 
 class BioFileSet(BioFile):
@@ -41,11 +88,11 @@ class BioFileSet(BioFile):
     ----------
 
     file_set_id : str or int
-      A unique ID that identifies the file set.
+        A unique ID that identifies the file set.
 
     files : [:py:class:`bob.bio.base.database.BioFile`]
-      A non-empty list of BioFile objects that should be stored inside this file.
-      All files of that list need to have the same client ID.
+        A non-empty list of BioFile objects that should be stored inside this file.
+        All files of that list need to have the same client ID.
     """
 
     def __init__(self, file_set_id, files, path=None, **kwargs):
@@ -56,7 +103,9 @@ class BioFileSet(BioFile):
         super(BioFileSet, self).__init__(
             files[0].client_id,
             "+".join(f.path for f in files) if path is None else path,
-            file_set_id, **kwargs)
+            file_set_id,
+            **kwargs,
+        )
 
         # check that all files come from the same client
         assert all(f.client_id == self.client_id for f in files)
