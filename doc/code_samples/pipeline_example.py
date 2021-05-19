1abf2d9a674346d40255e7a37efc8c2b8c1a4a22
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2021-05-19 19:07:01 +0200

    revert the flatten transformer implementation to provide an example

--- doc/code_samples/pipeline_example.py
+++ doc/code_samples/pipeline_example.py
@@ -2,18 +2,26 @@ import numpy as np
 from bob.bio.base.pipelines.vanilla_biometrics import BioAlgorithm
 from bob.bio.base.pipelines.vanilla_biometrics import VanillaBiometricsPipeline
 from bob.pipelines import wrap
-from bob.pipelines.transformers import Linearize
 from sklearn.decomposition import PCA
 from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.utils import check_array
 
 ## Transformers
 pca = PCA(n_components=0.95)
 
 # the images are in shape of Nx112x92, we want to flatten to Nx10304 them so we can train a PCA on them.
-linearize = Linearize()
+# A similar implementation is available in:
+# from bob.pipelines.transformers import Linearize
+def flatten(images):
+    images = check_array(images, allow_nd=True)
+    new_shape = [images.shape[0], -1]
+    return np.reshape(images, new_shape)
+
+flatten_transformer = FunctionTransformer(flatten, validate=False)
 
 # Chain the Transformers together
-transformer = make_pipeline(linearize, pca)
+transformer = make_pipeline(flatten_transformer, pca)
 
 # All transformers must be sample transformers
 transformer = wrap(["sample"], transformer)
