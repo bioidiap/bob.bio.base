ab82736d4832114f2ec7e5e28bf9ebcad9161f00
Yannick DAYER <yannick.dayer@idiap.ch>
2022-06-09 16:03:50 +0200

    Remove preprocessor, extractor, and algorithm.

--- setup.py
+++ setup.py
@@ -75,21 +75,6 @@ setup(
             "dummy             = bob.bio.base.test.dummy.database:database",  # for test purposes only
             "atnt              = bob.bio.base.config.database.atnt:database",
         ],
-        "bob.bio.preprocessor": [
-            "dummy             = bob.bio.base.test.dummy.preprocessor:preprocessor",  # for test purposes only
-            "filename          = bob.bio.base.config.preprocessor.filename:preprocessor",
-            "dummy_metadata    = bob.bio.base.test.dummy.preprocessor:preprocessor_metadata",
-        ],
-        "bob.bio.extractor": [
-            "dummy             = bob.bio.base.test.dummy.extractor:extractor",  # for test purposes only
-            "dummy_metadata    = bob.bio.base.test.dummy.extractor:extractor_metadata",  # for test purposes only
-            "linearize         = bob.bio.base.config.extractor.linearize:extractor",
-        ],
-        "bob.bio.algorithm": [
-            "distance-euclidean = bob.bio.base.config.algorithm.distance_euclidean:algorithm",
-            "distance-cosine   = bob.bio.base.config.algorithm.distance_cosine:algorithm",
-            "distance-hamming   = bob.bio.base.config.algorithm.distance_hamming:algorithm",
-        ],
         # main entry for bob bio cli
         "bob.cli": [
             "bio               = bob.bio.base.script.bio:bio",
