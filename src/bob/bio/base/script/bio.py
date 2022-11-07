080db3b83dde060654705b81d4a2df119afecd7a
Andre Mayoraz <andre.mayoraz@idiap.ch>
2022-11-07 17:35:31 +0100

    [click] Replaced aliased group from bob.extension to exposed

--- src/bob/bio/base/script/bio.py
+++ src/bob/bio/base/script/bio.py
@@ -4,8 +4,7 @@ import click
 import pkg_resources
 
 from click_plugins import with_plugins
-
-from bob.extension.scripts.click_helper import AliasedGroup
+from exposed.click import AliasedGroup
 
 
 @with_plugins(pkg_resources.iter_entry_points("bob.bio.cli"))
