172855ff1a02232a757ce1abd52b3ad5b7219b7e
Andre Mayoraz <andre.mayoraz@idiap.ch>
2022-11-08 11:28:09 +0100

    Removed bob.extension mention in init file

--- src/bob/bio/base/__init__.py
+++ src/bob/bio/base/__init__.py
@@ -10,13 +10,5 @@ from . import pipelines  # noqa: F401
 from . import script  # noqa: F401
 from . import score  # noqa: F401
 
-
-def get_config():
-    """Returns a string containing the configuration information."""
-    import bob.extension
-
-    return bob.extension.get_config(__name__)
-
-
 # gets sphinx autodoc done right - don't remove it
 __all__ = [_ for _ in dir() if not _.startswith("_")]
