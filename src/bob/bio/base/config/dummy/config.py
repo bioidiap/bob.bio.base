e6102d2791a675d1413dc07cbc472ec930e7eb8b
Yannick DAYER <yannick.dayer@idiap.ch>
2022-11-10 18:30:29 +0100

    [config] Fix the config files imports

--- src/bob/bio/base/config/dummy/config.py
+++ src/bob/bio/base/config/dummy/config.py
@@ -1,7 +1,7 @@
-from tests.dummy.algorithm import algorithm  # noqa: F401
-from tests.dummy.database import database  # noqa: F401
-from tests.dummy.extractor import extractor  # noqa: F401
-from tests.dummy.preprocessor import preprocessor  # noqa: F401
+from bob.bio.base.config.dummy.algorithm import algorithm  # noqa: F401
+from bob.bio.base.config.dummy.database import database  # noqa: F401
+from bob.bio.base.config.dummy.extractor import extractor  # noqa: F401
+from bob.bio.base.config.dummy.preprocessor import preprocessor  # noqa: F401
 
 zt_norm = True
 verbose = 1
